063ddcbe2fd1f812ee179f0678de89650b3b3b6d
eag1ex <eaglex@eaglex.net>
2020-03-16 17:30:21 +0700

    add index.js

---
+++ examples.js
@@ -0,0 +1,3 @@
+
+var Xpromise1 = require('./examples/xpromise-example.1')()
+// var pipeExamples = require('./examples/pipes-example.2')()
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
-var Xpromise1 = require('./examples/xpromise-example.1')()
-// var pipeExamples = require('./examples/pipes-example.2')()
+/**
+ * examples available at `examples.js`
+ */
+exports.XPromise = require('../xpromise/x.promise')()
+exports.notify = require('../libs/notifications')()
+// exports.XPipe = require('../xpromise/x.promise')() << follow example at `./examples/pipes-simple.js` to know how to integrate pipes
